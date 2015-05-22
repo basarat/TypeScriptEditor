b07eaad2b437bb01ee63bc9486773e71b1acc330
Basarat Syed <basaratali@gmail.com>
2015-05-22 20:10:26 +1000

    chore(ts) another file moved to ts

--- scripts/lib/ace/mode/typescript/DocumentPositionUtil.js
+++ scripts/lib/ace/mode/typescript/DocumentPositionUtil.js
@@ -1,28 +1,18 @@
-//define(function(require, exports, module) {
-//
-//
-//});
-
-define(function(require, exports, module) {
+define(["require", "exports"], function (require, exports) {
     var DocumentPositionUtil;
-    DocumentPositionUtil = (function() {
-
-        function DocumentPositionUtil() {}
-
-        DocumentPositionUtil.getLinesChars = function(lines) {
+    (function (DocumentPositionUtil) {
+        DocumentPositionUtil.getLinesChars = function (lines) {
             var count;
             count = 0;
-            lines.forEach(function(line) {
+            lines.forEach(function (line) {
                 return count += line.length + 1;
             });
             return count;
         };
-
-        DocumentPositionUtil.getChars = function(doc, pos) {
+        DocumentPositionUtil.getChars = function (doc, pos) {
             return DocumentPositionUtil.getLinesChars(doc.getLines(0, pos.row - 1)) + pos.column;
         };
-
-        DocumentPositionUtil.getPosition = function(doc, chars) {
+        DocumentPositionUtil.getPosition = function (doc, chars) {
             var count, i, line, lines, row;
             lines = doc.getAllLines();
             count = 0;
@@ -43,10 +33,5 @@ define(function(require, exports, module) {
                 column: chars - count
             };
         };
-
-        return DocumentPositionUtil;
-
-    }).call(this);
-    exports.DocumentPositionUtil = DocumentPositionUtil;
-    return exports;
+    })(DocumentPositionUtil = exports.DocumentPositionUtil || (exports.DocumentPositionUtil = {}));
 });
